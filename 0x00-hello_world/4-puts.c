0768a706fefef966724eba08b0e041f13600198b
halaalharaami <tasneemabrahams99@gmail.com>
2022-06-06 10:08:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - entry point of program
+*
+*description - Program to print output using put>
+*
+*Return: Always 0 Success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
